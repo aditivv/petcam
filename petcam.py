fe42c737f4718e7e70e2d6c490331bf84866b429
aditivv <140194386+aditivv@users.noreply.github.com>
2023-07-26 10:19:21 -0700

    Update petcam.py

--- petcam.py
+++ petcam.py
@@ -4,7 +4,7 @@
 from jetson_inference import imageNet
 from jetson_utils import videoSource, videoOutput
 
-# setting up detectnet and camera as video source
+# setting up imagenet and camera as video source
 net = imageNet("ssd-mobilenet-v2",threshold=0.5)
 camera = videoSource("/dev/video0")
 display = videoOutput("file://my_petcam.mp4")
