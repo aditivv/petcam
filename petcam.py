9d8ee9d2db5d218acdfabf5239194d866c6d1bda
aditivv <140194386+aditivv@users.noreply.github.com>
2023-07-27 08:55:23 -0700

    Update petcam.py

--- petcam.py
+++ petcam.py
@@ -1,14 +1,14 @@
-#!/usr/bin/python3
-
-# importing needed stuff
-from jetson_inference import imageNet
-from jetson_utils import videoSource, videoOutput
-
-# setting up imagenet and camera as video source
-net = imageNet("ssd-mobilenet-v2",threshold=0.5)
-camera = videoSource("/dev/video0")
-display = videoOutput("file://my_petcam.mp4")
-
-# live camera
-while display.IsStreaming():
-	
+import time
+import Jetson.GPIO as GPIO
+buz = 12
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(buz, GPIO.OUT,initial=GPIO.LOW)
+i = input()
+while True:
+	if i == '1':
+		GPIO.output(buz,GPIO.HIGH)
+	else:
+		GPIO.output(buz,GPIO.LOW)
+	i = input()
+# GPIO.output(buz, GPIO.HIGH)
+GPIO.cleanup()
